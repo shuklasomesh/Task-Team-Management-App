04393f2ec2cdf7fa676fe2f2d9e6cd926b0133e3
Somesh Shukla <someshshukla263@gmail.com>
2026-05-04 12:32:35 +0530

    fix: bind nginx to Railway's \ instead of hardcoded 80

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Substitute $BACKEND_URL into nginx config at container startup
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
